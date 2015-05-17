1f741b6d22d21382dd614c861ff8a7c296834c79
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-05-17 18:26:20 -0400

    Do exercise 3.12

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -88,6 +88,7 @@ object List { // `List` companion object. Contains functions for creating and wo
   
   def sumWithFoldLeft(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
   def productWithFoldLeft(l: List[Double]): Double = foldLeft(l, 1.0)(_ * _)
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A])((rev,a)=>Cons(a,rev))
 
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
--- exercises/worksheets/dataStructuresWorksheet.sc
+++ exercises/worksheets/dataStructuresWorksheet.sc
@@ -5,4 +5,5 @@ object dataStructuresWorksheet {
   length(List(1,2,3))                             //> res0: Int = 3
   sumWithFoldLeft(List(1,2,3))                    //> res1: Int = 6
   productWithFoldLeft(List(1.0, 4.0, 2.5))        //> res2: Double = 10.0
+  reverse(List(1,2,3))                            //> res3: fpinscala.datastructures.List[Int] = Cons(3,Cons(2,Cons(1,Nil)))
 }
